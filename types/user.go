d819cf19f6610ed4adf1a173d86e67df6adf97ce
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-01-24 19:25:41 +0300

    fix: omit empty values from update, leaving old state instead

--- types/user.go
+++ types/user.go
@@ -16,8 +16,8 @@ var (
 )
 
 type UpdateUserParams struct {
-	FirstName string `bson:"firstName" json:"firstName"`
-	LastName  string `bson:"lastName" json:"lastName"`
+	FirstName string `bson:"firstName,omitempty" json:"firstName"`
+	LastName  string `bson:"lastName,omitempty" json:"lastName"`
 }
 
 type InsertUserParams struct {
