cae2ea94ea5d199d1e88cba47453453360a76319
Aimen Sahnoun <aimensahnoun@outlook.com>
2024-02-17 18:46:24 +0300

    feat: implement JWT validation in middleware

--- api/middleware/jwt.go
+++ api/middleware/jwt.go
@@ -1,13 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
-
 )
 
 type Claims struct {
@@ -17,36 +17,60 @@ type Claims struct {
 
 func JWTAuthentication(c *fiber.Ctx) error {
 	if strings.Contains(c.Path(), "/auth") {
-		println("Skipping JWT for auth purposes")
-
 		return c.Next()
 	}
 
-	println("--- JWT AUTH ---")
+	authHeader := c.Get("Authorization")
+	if authHeader == "" {
+		return c.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
+	}
+
+	tokenString := strings.Split(authHeader, " ")[1]
+
+	claims, err := parseJWT(tokenString)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).SendString("Unauthorized")
+	}
+
+	println("Authenticated email:", claims.Email)
 
 	return c.Next()
 }
 
-func GenerateJWT(email string) (string ,error){
+func GenerateJWT(email string) (string, error) {
 	jwtSecret := []byte(os.Getenv("JWT_SECRET"))
 
 	expiration := time.Now().Add(7 * 24 * time.Hour)
 
-claims:= &Claims{
-    Email : email,
-    StandardClaims: jwt.StandardClaims{
-      ExpiresAt: expiration.Unix(),
-    },
-  } 
+	claims := &Claims{
+		Email: email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expiration.Unix(),
+		},
+	}
 
-  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-  tokenString , err := token.SignedString(jwtSecret)
-  
-  if err != nil {
-    return "" ,err
-  }
+	tokenString, err := token.SignedString(jwtSecret)
 
+	if err != nil {
+		return "", err
+	}
 
-  return tokenString , nil
+	return tokenString, nil
+}
+
+func parseJWT(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, fmt.Errorf("invalid token")
+	}
 }
